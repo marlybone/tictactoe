fb7f4f58e1d81b218a0aa473d0f91463e6316ac0
Phonique <41431476+marlybone@users.noreply.github.com>
2022-12-03 20:48:06 +0000

    added skeleton functions and some logic.

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-const displayStatus = document.querySelector('.game--status')
+const displayStatus = document.querySelector('.game--status') 
+
+let gameActive = true;
+
+let currentPlayer = 'X';
+
+let gameState = ["","","","","","","","",""];
+
+const winningMessage = () => `Player ${currentPlayer} has Won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+displayStatus.innerHTML = currentPlayerTurn();
+
+function handleCellPlayed() {
+
+  
+}
+
+function handleResults() {
+
+  
+}
+
+function handleCellClick() {
+  const clickedCell = clickedCellEvent.target
+  
+}
+
+function handleRestartGame() {
+
+  
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleClickCell));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+const clickedCellIndex = parseInt(
+  clickedCell.getAttribute('data-cell-index')
+);
+
+if (gameState[clickedCellIndex] !== "" || !gameActive) {
+  return;
+}
+
